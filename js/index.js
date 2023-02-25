33a9d724354974a9968d965fd958ecf22dc44d6c
mayurac7 <chhatwanimayur7744@gmail.com>
2023-02-25 20:28:03 +0530

    made changes

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-typed", {
+    strings: ["Full Stack Developer","Data Analyst", "Software Developer", "Data Engineer","Machine Learning Engineer","Tech Enthusiast "],
+    typedSpeed: 150,
+    backSpeed: 50,
+    loop: true
+})
